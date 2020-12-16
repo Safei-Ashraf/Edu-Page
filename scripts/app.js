4cd74a025c9ffc0d0ecb78b1fb7c29b5bce96b10
Safei Ashraf <Safei.ashraf@gmail.com>
2020-12-16 04:05:41 +0200

    hide answers logic

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,7 @@
+//Global obj to hold data:
+let answerInfo = {};
+//Answers list:
+const modelAnswers = [`eraser`,`ruler`,`pencil`,`book`,`pen`];
 //Catching DOM elements:
 //top buttons:
 const imgBtn = document.querySelector('#dummy-img-btn');
@@ -20,6 +24,28 @@ const displayHelp = ()=>{
 }
 helpBtn.addEventListener('click', displayHelp);
 
-
+//event checks if answer is correct or wrong:
 answersList.forEach(option => option.addEventListener('click',
- ()=>{console.log('answer clicked', option)}));
+    ()=>{
+    console.log('answer clicked', option)
+    answerInfo.selected = option.textContent;
+    console.log(answerInfo.selected);
+    answerInfo.checked = modelAnswers.includes(answerInfo.selected);
+    answerInfo.option = option.id;
+    console.log(answerInfo.storedValue);
+}));
+
+answerAreas.forEach(box=> box.addEventListener('click', ()=>{
+    if(answerInfo.selected)
+    {
+        if(answerInfo.checked)
+        {
+            document.getElementById(answerInfo.option).classList.add('hide-answer');
+            box.classList.add('hide-answer');
+            box.textContent = answerInfo.selected;
+        }
+        else{
+            box.textContent = 'X'
+        }
+    }
+    }));
