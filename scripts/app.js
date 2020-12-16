523086f94978ae9c763640885f68a30293511bfd
Safei Ashraf <Safei.ashraf@gmail.com>
2020-12-16 03:15:21 +0200

    removing outline on btns click

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,16 @@
 //Catching DOM elements:
+//top buttons:
+const imgBtn = document.querySelector('#dummy-img-btn');
+const helpBtn = document.querySelector('#help-btn');
+
+//Answer options:
+const answersList = document.querySelectorAll('.option');
+
+//Answer Areas:
+const answerAreas = document.querySelectorAll('.answer-place');
+
+//Handle Click Events:
+imgBtn.addEventListener('click', imgDisplay);
+const imgDisplay = ()=>{
+
+}
